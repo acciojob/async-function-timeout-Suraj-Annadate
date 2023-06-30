22431e4fcb134933d8eafee8c66bcae422e36e44
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-30 15:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+    const output = document.getElementById('output');
+
+    async function displayTextWithDelay() {
+      const text = document.getElementById('text').value;
+      const delay = document.getElementById('delay').value;
+
+      // Validate input
+      if (!text || !delay) {
+        output.textContent = 'Please provide both text and delay values.';
+        return;
+      }
+
+      output.textContent = 'Waiting for the delay...';
+
+      // Convert delay to a number
+      const delayMilliseconds = Number(delay);
+
+      // Wait for the specified delay
+      await new Promise(resolve => setTimeout(resolve, delayMilliseconds));
+
+      // Display the text after the delay
+      output.textContent = text;
+    }
